75d2658a04ea0224452da177678dd79168392d2a
Lebo1094 <125014406+Lebo1094@users.noreply.github.com>
2024-06-19 20:03:53 +0200

    project/API integration

--- src/Search.js
+++ src/Search.js
@@ -1,19 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import Weather from "./Weather";
+import axios from "axios";
 
 import "./Search.css";
 
 export default function Search() {
+  const [city, setCity] = useState(null);
+  const [data, setData] = useState({});
+
+  function showTempData(response) {
+    setData({
+      city: response.data.name,
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      feels_like: response.data.main.feels_like,
+      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`,
+    });
+  }
+
+  function updateCity(event) {
+    event.preventDefault();
+    setCity(event.target.value);
+  }
+
+  function getWeatherData(event) {
+    event.preventDefault();
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2718952144ed077c12e7c160fb6fc351&units=metric`;
+    axios.get(apiUrl).then(showTempData);
+  }
+
   return (
     <div className="Search">
       <h1>Weather Today</h1>
-      <form>
+      <form onSubmit={getWeatherData}>
         <div className="row">
           <div className="col-9">
             <input
               type="search"
               placeholder="Enter a City"
               autoFocus
+              onChange={updateCity}
               className="form-control"
             />
           </div>
@@ -22,7 +50,7 @@ export default function Search() {
           </div>
         </div>
       </form>
-      <Weather />
+      <Weather weatherData={data} />
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,44 +1,65 @@
 import React from "react";
+import { Rings } from "react-loader-spinner";
 
 import "./Weather.css";
 
 export default function Weather({ weatherData }) {
+  if (!weatherData.city) {
+    return (
+      <div className="loading-spinner">
+        <Rings
+          visible={true}
+          height="80"
+          width="80"
+          color="#4fa94d"
+          ariaLabel="rings-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+        />
+      </div>
+    );
+  }
+
   return (
     <div className="Weather">
       <h2>Current Weather</h2>
       <div className="row">
-        <div className="col-5">
+        <div className="col-4">
           <h5>
-            city
+            {weatherData.city}
             <br />
             Tuesday 20:30
           </h5>
         </div>
         <div className="col-4">
           <h5>
-            <strong>temperature°C</strong>
+            <strong>{Math.round(weatherData.temperature)}°C</strong>
             <br />
-            <strong>description</strong>{" "}
+            <strong class="text-capitalize">
+              {weatherData.description}
+            </strong>{" "}
           </h5>
         </div>
-        <div className="col-3">icon</div>
+        <div className="col-4">
+          <img src={weatherData.iconUrl} alt="Weather Icon" />
+        </div>
       </div>
       <br />
       <h2>Air Conditions</h2>
       <div className="row">
         <div className="col-4">
           <h5>
-            Humidity <br /> <strong>humidity%</strong>
+            Feels like <br /> {Math.round(weatherData.feels_like)}℃
           </h5>
         </div>
         <div className="col-4">
           <h5>
-            Wind <br /> <strong>wind km/h</strong>
+            Humidity <br /> <strong>{weatherData.humidity}%</strong>
           </h5>
         </div>
         <div className="col-4">
           <h5>
-            Precipitation <br /> rain%
+            Wind <br /> <strong>{weatherData.wind} km/h</strong>
           </h5>
         </div>
       </div>

b2bb385d0f69fb972cc375d58393b6fba270f72a
Lebo1094 <125014406+Lebo1094@users.noreply.github.com>
2024-06-20 20:46:14 +0200

    added links in the footer

--- src/App.js
+++ src/App.js
@@ -7,7 +7,24 @@ function App() {
       <div className="container">
         <Search />
       </div>
-      <footer>Coded by Lebogang Dingalo</footer>
+      <footer>
+        This project was coded by <a href="/"> Lebogang Dingalo</a> and is{" "}
+        <a
+          href="https://github.com/Lebo1094/my-react-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://ld-react-project.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </footer>
     </div>
   );
 }
